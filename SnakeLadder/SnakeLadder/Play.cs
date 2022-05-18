6f03cf5ca9f45b346efe0867e6f840c34c206fb3
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-18 19:54:18 +0530

    [Suraj]: Usecase 7 to Play 2 player Snake and Ladder game is added.

--- SnakeLadder/SnakeLadder/Play.cs
+++ SnakeLadder/SnakeLadder/Play.cs
@@ -8,17 +8,18 @@ namespace SnakeLadder
 {
     internal class Play
     {
-        int position1 = 0, count = 0;
-        int Player1;
-
+        int position1 = 0, position2 = 0, count = 0;
+        int Player1, Player2;
         public void rollDice()
         {
-            Random random = new Random();
 
-            while (position1 != 100)
+            Random random = new Random();
+            while (position1 != 100 && position2 != 100)
             {
                 Player1 = random.Next(0, 7);
+                Player2 = random.Next(0, 7);
                 int check1 = random.Next(0, 3);
+                int check2 = random.Next(0, 3);
                 count++;
 
                 switch (check1)
@@ -32,13 +33,38 @@ namespace SnakeLadder
                         position1 -= Player1;
                         break;
                 }
+
                 if (position1 < 0)
                     position1 = 0;
                 if (position1 > 100)
                     position1 -= Player1;
-                Console.WriteLine("Dice roll: " + count + " Position of Player 1: " + position1);
+
+                switch (check2)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        position2 += Player2;
+                        break;
+                    case 2:
+                        position2 -= Player2;
+                        break;
+                }
+
+                if (position2 < 0)
+                    position2 = 0;
+                if (position2 > 100)
+                    position2 -= Player2;
+
+                Console.WriteLine("Position of Player 1: " + position1);
+                Console.WriteLine("Position of Player 2: " + position2);
             }
+            if (position1 == 100)
+                Console.WriteLine("Player 1 won the match.");
+            else
+                Console.WriteLine("Player 2 won the match.");
             Console.WriteLine("Dice rolled for " + count + " no. of time.");
+
         }
     }
 }
