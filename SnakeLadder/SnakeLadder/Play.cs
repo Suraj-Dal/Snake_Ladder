a3dd9c5a48c5d248a450bcd43d238fb36ca8eec3
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-18 19:20:51 +0530

    [Suraj]: Usecase 1 for start position is added.

---
+++ SnakeLadder/SnakeLadder/Play.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Play
+    {
+        int position1 = 0;
+        int Player1;
+    }
+}
